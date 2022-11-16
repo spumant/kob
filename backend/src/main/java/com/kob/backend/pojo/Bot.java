5d106f317a19772378c09c500e4af278a1d4d866
淏喆 邢 <253383985@qq.com>
2022-11-16 23:11:19 +0800

    创建个人中心页面

---
+++ backend/src/main/java/com/kob/backend/pojo/Bot.java
@@ -0,0 +1,27 @@
+package com.kob.backend.pojo;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.yaml.snakeyaml.events.Event;
+
+import java.util.Date;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Bot {
+    @TableId(type = IdType.AUTO)
+    private Integer id;//在pojo里最好使用引用类型
+    private Integer userId;//pojo里使用驼峰命名法与数据库中的下划线对应
+    private String title;
+    private String description;
+    private String content;
+    private int rating;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Shanghai")
+    private Date createtime;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Shanghai")
+    private Date modifytime;
+}
