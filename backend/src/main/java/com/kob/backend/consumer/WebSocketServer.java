bb7c803020b6f9f2beef4ee6e57aaa65ccc5e314
淏喆 邢 <253383985@qq.com>
2022-11-17 19:43:17 +0800

    实现微服务：匹配系统（上）

---
+++ backend/src/main/java/com/kob/backend/consumer/WebSocketServer.java
@@ -0,0 +1,128 @@
+package com.kob.backend.consumer;
+
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.kob.backend.consumer.utils.Game;
+import com.kob.backend.consumer.utils.JwtAuthentication;
+import com.kob.backend.mapper.UserMapper;
+import com.kob.backend.pojo.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.websocket.*;
+import javax.websocket.server.PathParam;
+import javax.websocket.server.ServerEndpoint;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArraySet;
+
+@Component
+@ServerEndpoint("/websocket/{token}")  // 注意不要以'/'结尾
+public class WebSocketServer {
+    //线程安全的哈希表，将userId映射到相应用户的WebSocketServer
+    final private static ConcurrentHashMap<Integer,WebSocketServer> users=new ConcurrentHashMap<>();
+    final private static CopyOnWriteArraySet<User> matchpool =new CopyOnWriteArraySet<>();
+    private User user;//存储每个链接对应的用户信息
+    //每个链接都是用session来维护的
+    private Session session=null;
+
+    private static UserMapper userMapper;
+
+//    private Game game=null;
+
+    @Autowired
+    public void setUserMapper(UserMapper userMapper){
+        WebSocketServer.userMapper=userMapper;
+    }
+
+    @OnOpen
+    public void onOpen(Session session, @PathParam("token") String token) throws IOException {
+        // 建立连接
+        this.session=session;
+        System.out.println("connected");
+        //当建立连接时，需要从token中读取当前的用户是谁，为了方便，一开始传入的不是token而是id
+
+        //现在已经修改为使用token登录
+        Integer userId= JwtAuthentication.getUserId(token);
+        this.user=userMapper.selectById(userId);
+        if(this.user!=null){//如果这名用户存在则表示链接成功
+            users.put(userId,this);
+        }else{//否则断开连接
+            this.session.close();
+        }
+
+    }
+
+    @OnClose
+    public void onClose() {
+        // 关闭链接
+        System.out.println("disconnected");
+        //当关闭链接时需要将user移除
+        if(this.user!=null){
+            users.remove(this.user.getId());
+            matchpool.remove(this.user);
+        }
+    }
+
+    private void startMatching(){
+        matchpool.add(this.user);
+        while (matchpool.size()>=2){
+            Iterator<User> it= matchpool.iterator();
+            User a=it.next();User b=it.next();
+            matchpool.remove(a);
+            matchpool.remove(b);
+
+            Game game=new Game(13,14,20);
+            game.CreateMap();
+
+            JSONObject respA=new JSONObject();//用于将A的消息传回
+            respA.put("event","start-matching");
+            respA.put("opponent_username",b.getUsername());//传回对手的名字
+            respA.put("opponent_photo",b.getPhoto());//传回对手的头像
+            respA.put("gamemap",game.getG());
+            users.get(a.getId()).sendMessage(respA.toJSONString());//获取A的链接，将A的信息传到前端
+
+            JSONObject respB=new JSONObject();//用于将B的消息传回
+            respB.put("event","start-matching");
+            respB.put("opponent_username",a.getUsername());//传回对手的名字
+            respB.put("opponent_photo",a.getPhoto());//传回对手的头像
+            respB.put("gamemap",game.getG());
+            users.get(b.getId()).sendMessage(respB.toJSONString());//获取B的链接，将B的信息传到前端
+        }
+    }
+
+    private void stopMatching(){
+        matchpool.remove(this.user);
+    }
+
+    @OnMessage
+    public void onMessage(String message, Session session) {//当作路由判断要将任务交给谁
+        // 从Client接收消息
+        System.out.println("receive message!");
+        JSONObject data= JSON.parseObject(message);
+        String event=data.getString("event");
+        if("start-matching".equals(event)){
+            startMatching();
+        }else if("stop-matching".equals(event)){
+            stopMatching();
+        }
+    }
+
+    @OnError
+    public void onError(Session session, Throwable error) {
+        error.printStackTrace();
+    }
+
+    public void sendMessage(String message){//实现从后端向前端发送信息
+        synchronized (this.session) {//异步通信需要加锁
+            try {
+                this.session.getBasicRemote().sendText(message);//这个api可以将后端的信息发送给前端
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+}
